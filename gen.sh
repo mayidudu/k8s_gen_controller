2acd3591191526a3877c5abb905330279128cf01
mayidudu <316148732@qq.com>
2020-08-13 19:29:36 +0800

    just go get

--- gen.sh
+++ gen.sh
@@ -1,8 +1,3 @@
 cd $GOPATH/src \
 && go get -u k8s.io/apimachinery/pkg/apis/meta/v1 \
-&& go get -u k8s.io/code-generator/... \
-&& cd $GOPATH/src/k8s.io/code-generator \
-&& ./generate-groups.sh all \
-dudu_controller/pkg/client \
-dudu_controller/pkg/apis \
-dudu:v1
+&& go get -u k8s.io/code-generator/... 
